e2e14df26b26c9897c96e3075220a93cb7cd6da6
SoumyaCMC <soumya133.ranjan@gmail.com>
2021-02-12 21:17:14 +0530

    Update Profile.js
    minor change to non-functional tab bar

--- my-app/src/components/screens/Profile.js
+++ my-app/src/components/screens/Profile.js
@@ -36,8 +36,8 @@ const Profile = () => {
          <div class="row">
     <div class="col s12">
       <ul class="tabs">
-        <li class="tab col s3"><a href="#test1">Test 1</a></li>
-        <li class="tab col s3"><a class="active" href="#test2">Test 2</a></li>
+        <li class="tab col s3"><a href="#test1">Details</a></li>
+        <li class="tab col s3"><a class="active" href="#test2">Stats</a></li>
       </ul>
     </div>
     <div id="test1" class="col s12">Test 1</div>
