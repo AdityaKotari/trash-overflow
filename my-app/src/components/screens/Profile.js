b13f50847b4fa840716e5d4f33149e9f3ac5a43d
SoumyaCMC <soumya133.ranjan@gmail.com>
2021-02-13 09:34:56 +0530

    fully updated profile page

--- my-app/src/components/screens/Profile.js
+++ my-app/src/components/screens/Profile.js
@@ -1,22 +1,41 @@
 import React from 'react'; 
 import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
 
 import './Profile.css'
 
 
 const profileBanner ={
-    height: '500px',
+    height: '461px',
     width: '100%',
-    background: 'rgb(26,35,126)',
     background: 'linear-gradient(135deg, rgba(26,35,126,1) 0%, rgba(87,99,230,1) 100%)',
 }
 
 const imgStyle = {
     padding: '30px',
-    height: '375px',
-    borderRadius: '300px'
+    height: '275px',
+    borderRadius: '200px'
   };
+
+const TabListStyle ={
+    borderBottom: '3px solid rgba(87,99,230,1)',
+    margin: '0 0 10px',
+    padding: '10px',
+}
+
+const TabStyle ={
+    display: 'inline-block',
+    border: '1px solid transparent',
+    width: '50%',
+    borderBottom: 'none',
+    bottom: '0',
+    position: 'relative',
+    listStyle: 'none',
+    padding: '6px 12px',
+    cursor: 'pointer',
+    color: 'white',
+    font: 'Roboto',
+    textAlign: 'center',
+}
   
 
 const Profile = () => {
@@ -36,30 +55,56 @@ const Profile = () => {
                 <div className='center-align' style={{color: 'white', font: 'Roboto'}}><h3><p>John Doe</p></h3></div>
                 </div>
         </div>
-        </div>
-        <Tabs defaultIndex={1} onSelect={index => console.log(index)}>
-            <TabList>
-                <Tab>Details</Tab>
-                <Tab>Stats</Tab>
+        <Tabs defaultIndex={0} onSelect={index => console.log(index)}>
+            <TabList style={TabListStyle}>
+                <Tab style={TabStyle}>Details</Tab>
+                <Tab style={TabStyle}>Stats</Tab>
             </TabList>
             <TabPanel>
-                Random Stuff
+                <div>
+                    <form className='row s12'>
+                        <div className='row'>
+                            <div className='input-field col s12'>
+                            <i class="material-icons prefix">home</i>
+                                <input placeholder='First Address Line' id='first_address_line' type='text' class='validate'></input>
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='input-field col s12'>
+                            <i class="material-icons prefix">home</i>
+                                <input placeholder='Second Address Line' id='second_address_line' type='text' class='validate'></input>
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='input-field col s12'>
+                            <i class="material-icons prefix">phone</i>
+                                <input placeholder='Contact Number' id='phone' type='tel' class='validate'></input>
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='input-field col s12'>
+                            <i class="material-icons prefix">email</i>
+                                <input placeholder='Email' id='email' type='email' class='validate'></input>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </TabPanel>
             <TabPanel>
             <div className='row'>
                 <div class="col s6 offset-by-3">
                     <div class="card pink lighten-5">
                     <div class="card-content black-text">
-                    <span class="card-title">42</span>
-                    <p>Spots Cleaned Up Yet</p>
+                    <span class="card-title"><div style={{textAlign:'center'}}><b>42</b></div></span>
+                    <div style={{textAlign:'center'}}><p>Spots Cleaned Up Yet</p></div>
                     </div>
                 </div>    
                 </div> 
                 <div class="col s6 offset-by-3">
                 <div class="card pink lighten-5">
                     <div class="card-content black-text">
-                    <span class="card-title">10,000</span>
-                    <p>Rewards Earned Till Now</p>
+                    <span class="card-title"><div style={{textAlign:'center'}}><b>10,000</b></div></span>
+                    <div style={{textAlign:'center'}}><p>Rewards Earned Till Now</p></div>
                     </div>
                 </div>
                 </div>
@@ -68,22 +113,24 @@ const Profile = () => {
             <div class="col s6 offset-by-3">
                 <div class="card pink lighten-5">
                     <div class="card-content black-text">
-                    <span class="card-title">2</span>
-                    <p>Top Position on Leaderboard</p>
+                    <span class="card-title"><div style={{textAlign:'center'}}><b>2</b></div></span>
+                    <div style={{textAlign:'center'}}><p>Leaderboard Position</p></div>
                     </div>
                 </div>
             </div>
             <div class="col s6 offset-by-3">
                 <div class="card pink lighten-5">
                     <div class="card-content black-text">
-                    <span class="card-title">56,000</span>
-                    <p>Payment Received</p>
+                    <span class="card-title"><div style={{textAlign:'center'}}><b>56,000</b></div></span>
+                    <div style={{textAlign:'center'}}><p>Payment Received</p></div>
                     </div>
                 </div>
             </div>
         </div>
             </TabPanel>
         </Tabs>
+        </div>
+
         
 
         
