9fb3c0007d1baf0c38dfc4782f82e2d263d468ce
guilefoylegaurav <grvb1515@gmail.com>
2021-02-13 17:34:08 +0530

    Adding fetch API implemetations for making POST requests to signup and login routes in backend. #2

--- my-app/src/components/LogIn/LogIn.js
+++ my-app/src/components/LogIn/LogIn.js
@@ -1,6 +1,42 @@
-import React from 'react';
 
+import React,{useState,useContext,} from 'react'
+import {Link,NavLink,useHistory} from 'react-router-dom'
+import M from 'materialize-css'
 const LogIn = () =>{
+    const history = useHistory()
+    const [password,setPasword] = useState("")
+    const [email,setEmail] = useState("")
+    const uploadFields = ()=>{
+        if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
+            M.toast({html: "Please enter a valid email",classes:"#c62828 red darken-3"})
+            return
+        }
+        fetch("/profile/login",{
+            method:"post",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                password,
+                email
+            })
+        }).then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+           if(data.error){
+              M.toast({html: data.error,classes:"#c62828 red darken-3"})
+           }
+           else{
+               localStorage.setItem("jwt",data.token)
+               localStorage.setItem("user",JSON.stringify(data.user))
+            //    dispatch({type:"USER",payload:data.user})
+               M.toast({html:"Welcome!",classes:"#43a047 green darken-1"})
+               history.push('/')
+           }
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
     return (
     <main>
         <center>
@@ -12,30 +48,32 @@ const LogIn = () =>{
         <div className="container">
             <div className="z-depth-1 grey lighten-4 row" style={{display: "inline-block", padding: "32px 48px 0px 48px", border: "1px solid #EEE"}}>
 
-            <form className="col s12" method="post">
+            <form className="col s12" onSubmit={(e)=>{e.preventDefault()}}>
 
                 <div className='row'>
                 <div className='input-field col s12'>
-                    <input className='validate' type='email' name='email' id='email' />
-                    <label for='email'>Enter your email</label>
+                    <input className='validate' type='email'  value={email}
+            onChange={(e)=>setEmail(e.target.value)} />
+                    <label for='email'>Email</label>
                 </div>
                 </div>
 
                 <div className='row'>
                 <div className='input-field col s12'>
-                    <input className='validate' type='password' name='password' id='password' />
-                    <label for='password'>Enter your password</label>
+                    <input className='validate' type='password' value={password}
+            onChange={(e)=>setPasword(e.target.value)} />
+                    <label for='password'>Password</label>
                 </div>
                 </div>
 
                 <br />
                 <center>
                 <div className='row'>
-                    <button type='submit' name='btn_login' className='col s12 btn btn-large waves-effect indigo'>Login</button>
+                    <button type='submit' name='btn_login' className='col s12 btn btn-large waves-effect indigo'  onClick={()=>uploadFields()}>Login</button>
                 </div>
                 </center>
             </form>
-            <a href="#!">Create account</a>
+            <NavLink to="/signup">Create account</NavLink>
             <div className="section"></div>
             </div>
         </div>
--- my-app/src/components/SignUp/SignUp.js
+++ my-app/src/components/SignUp/SignUp.js
@@ -1,49 +1,103 @@
-import React from 'react';
+
+
+  
+import React,{useState,useEffect} from 'react';
+import {Link,NavLink,useHistory} from 'react-router-dom';
+import M from 'materialize-css';
 import './SignUp.css';
 
 const SignUp = () => {
+    const history = useHistory()
+    const [name,setName] = useState("")
+    const [password,setPassword] = useState("")
+    const [email,setEmail] = useState("")
+    const [phone,setPhone] = useState("")
+
+    const uploadFields = ()=>{
+        if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
+            M.toast({html: "invalid email",classes:"#c62828 red darken-3"})
+            return
+        }
+        fetch("/profile/signup",{
+            method:"post",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                name,
+                password,
+                email,
+                phone
+            })
+        }).then(res=>res.json())
+        .then(data=>{
+
+            console.log(data);
+           if(data.error){
+               console.log(data.error); 
+              M.toast({html: data.error,classes:"#c62828 red darken-3"})
+           }
+           else{
+               M.toast({html:data.message,classes:"#43a047 green darken-1"})
+               history.push('/login')
+           }
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+
     return (
     <main>
         <center>
         <div className="section"></div>
 
-        <h5 className="indigo-text">Please, create your account</h5>
+        <h5 className="indigo-text">Register</h5>
         <div className="section"></div>
 
         <div className="container">
             <div className="z-depth-1 grey lighten-4 row" style={{display: "inline-block", padding: "32px 48px 0px 48px", border: "1px solid #EEE"}}>
 
-            <form className="col s12" method="post" action="http://localhost:5000/profile/signup">
+            <form className="col s12" onSubmit={(e)=>{e.preventDefault()}}>
 
                 <div className='row'>
                 <div class="input-field col s12">
-                    <input id="name" type="text" class="validate" name="name" />
+                    <input id="name" type="text" class="validate" name="name" value={name}
+            onChange={(e)=>setName(e.target.value)}/>
                     <label for="name">Full Name</label>
                 </div>
                 </div>
 
                 <div className='row'>
                 <div className='input-field col s12'>
-                    <input className='validate' type='email' name='email' id='email' />
-                    <label for='email'>Enter your email</label>
+                    <input className='validate' type='email' name='email' id='email' value={email}
+            onChange={(e)=>setEmail(e.target.value)} />
+                    <label for='email'>Email address</label>
+                </div>
+                </div>
+                <div className='row'>
+                <div class="input-field col s12">
+                    <input id="phone" type="text" class="validate" name="phone" value={phone}
+            onChange={(e)=>setPhone(e.target.value)}/>
+                    <label for="phone">Contact number</label>
                 </div>
                 </div>
 
                 <div className='row'>
                 <div className='input-field col s12'>
-                    <input className='validate' type='password' name='password' id='password' />
-                    <label for='password'>Enter your password</label>
+                    <input className='validate' type='password' name='password' id='password'  value={password}
+            onChange={(e)=>setPassword(e.target.value)} />
+                    <label for='password'>Password</label>
                 </div>
                 </div>
 
                 <br />
                 <center>
                 <div className='row'>
-                    <button type='submit' className='col s12 btn btn-large waves-effect indigo'>SIGNUP</button>
+                    <button type='submit' className='col s12 btn btn-large waves-effect indigo'  onClick={()=>uploadFields()}>SIGNUP</button>
                 </div>
                 </center>
             </form>
-            <p>Already have an account? <a href="#!">Login</a></p>
+            <p>Already have an account? <NavLink to="/login">Login</NavLink></p>
             <div className="gap"></div>
             </div>
         </div>
