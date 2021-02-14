002c071a4e7ac226642f89de24ae2c3a336db9e7
guilefoylegaurav <grvb1515@gmail.com>
2021-02-14 16:19:27 +0530

    Revamped UI of Leaderboard

--- client/src/components/screens/Leaderboard.js
+++ client/src/components/screens/Leaderboard.js
@@ -31,9 +31,37 @@ const Leaderboard = () => {
     
     return(
     <div>
-        <div className='row' style={{padding: '5px', alignContent: 'center'}}><h2>Leaderboard</h2></div>
-        {items.map((user,index) => (
-            <div className="row">
+        <div className='row center'><h4>Leaderboard</h4>
+        <span style = {{fontWeight: "bold"}}>Our community of green warriors</span></div>
+       
+        {items.map((user) => (
+            <div class="row">
+            <div class="col s12 m7">
+              <div class="card" key = {user._id}>
+                <div class="card-image">
+                  <img src="https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" />
+                  <span class="card-title">{user.name}</span>
+                </div>
+                <div class="card-content">
+                  <p><span style = {{fontWeight: "bold", color: "orange"}} >SPOTS CLEANED: </span> {user.spotsCleaned} </p>
+                  <p><span style = {{fontWeight: "bold",  color: "orange"}}>PAYMENT RECEIVED: </span> {user.paymentRecieved} </p>
+                  
+                </div>
+               
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+); 
+
+
+ 
+}
+
+export default Leaderboard; 
+
+{/* <div className="row">
             <div className="col s12">
             <div className="card blue-grey darken-1" style={{height: '60vh'}}>
                 <div className="card-content white-text">
@@ -50,13 +78,4 @@ const Leaderboard = () => {
                 </div>
             </div>
             </div>
-        </div>
-        ))}
-    </div>
-); 
-
-
- 
-}
-
-export default Leaderboard; 
+        </div> */}
